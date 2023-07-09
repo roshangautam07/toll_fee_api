aa58690f8bae8c96128a2e20116cead7d11b85b0
roshan <roh@roshans-Mac-mini.local>
2023-07-09 14:50:58 +0545

    apk detail

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path ,{ dirname}  from 'path';
 import * as url from 'url';
+import Apk from "node-apk";
     const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('..', import.meta.url));
 const __dirnames = url.fileURLToPath(new URL('.', import.meta.url));
@@ -9,22 +10,26 @@ const __dirnames = url.fileURLToPath(new URL('.', import.meta.url));
 
 export const appUpdate = (req, res, next) => {
     const response = {
-        apkUrl: "https://github.com/mikehardy/react-native-update-apk/releases/download/v3.0.1/rn-update-apk-example-3.0.1.apk",
+        apkUrl: "http://143.110.254.245:9006/api/download/toll-fee-2.0.apk",
         forceUpdate: false,
-        versionCode: "1",
-        versionName: "1.0"
+        versionCode: "2",
+        versionName: "2.0"
     };
-    fs.readFile(`${__dirnames}/${req.params.id}`, (err, data) => {
-        if (err) return res.status(500).json({message:err})
-        let RE = JSON.parse(data);
-        return res.json(RE);
+    // fs.readFile(`${__dirnames}/${req.params.id}`, (err, data) => {
+    //     if (err) return res.status(500).json({message:err})
+    //     let RE = JSON.parse(data);
+    //     return res.json(RE);
 
-    });
+    // });
+    return res.json(response);
 }
 
-export const  downloadAPK = (req, res, next) =>{
+export const  downloadAPK = async(req, res, next) =>{
     const paths = `${__dirname}/public/static/`;
     const file = 'toll-fee-1.0.apk';
+    const apk = new Apk.Apk(paths + req.params.id);
+    const apkInfo = await apk.getManifestInfo();
+    console.log(apkInfo.versionCode);
   fs.access(paths, fs.constants.F_OK, function (error) {
     if (error) {
       return res.status(500).json({message:error})
@@ -45,4 +50,8 @@ export const  downloadAPK = (req, res, next) =>{
     
   });
     
+}
+
+export const uploadApk = (req, res, next) => {
+
 }
