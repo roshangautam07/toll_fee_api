bb7894adf0acdac24498dcd01b75886a7ded7c56
roshan <roh@roshans-Mac-mini.local>
2024-04-22 11:27:37 +0545

    configure url in env

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -42,7 +42,7 @@ export const appUpdate = async (req, res, next) => {
     const lastDeploument = await getLastDeployment();
     console.log(lastDeploument);
     const responseData = {
-        apkUrl: `http://202.51.1.204:9006/api/download/${lastDeploument.app_name}`,
+        apkUrl: `${process.env.BASE_URL}:${process.env.PORT}/api/download/${lastDeploument.app_name}`,
         forceUpdate: lastDeploument?.is_force_update,
         versionCode: lastDeploument?.versionCode,
         versionName: lastDeploument?.versionName
